b449ecec05df3197d66ad8043cb4f4658eb9607b
jorgeortiz41 <jorge.ortiz41@upr.edu>
2023-06-05 23:06:41 -0400

    added proof of work section header

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import {motion, useScroll, useSpring } from 'framer-motion'
 import { useEffect, useState } from 'react';
 import Description from '@/components/Description';
+import ProofWork from '@/components/ProofWork';
 import { LoremIpsum } from "../components/LoremIpsum";
  
 
@@ -140,12 +141,7 @@ export default function Home() {
                 whileTap={{ scale: 0.9 }}
                 transition={{ type: "spring", stiffness: 400, damping: 17 }}
               >
-                GitHub
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                </svg>
-                
-
+                GitHub<svg class="h-6 w-6 stroke-blue-500 m-0 stroke-2 fill-blue-500"  viewBox="0 0 24 24">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
               </motion.div>
             </div>
 
@@ -154,6 +150,10 @@ export default function Home() {
         {/* BELOW IS 2nd COLUMN-3rd ROW WITH PORTFOLIO DESCRIPTION */}
 
         <Description />
+
+        {/* BELOW IS 2nd COLUMN-4th ROW WITH THE PROOF OF WORK SECTION */}
+
+        <ProofWork />
 
       </div>
       
---
+++ components/ProofWork.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const descriptionVariants = {
+    offscreen: {
+        opacity: 0,
+        y: 20,
+    },
+    onscreen: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 100,
+            delay: 0.3,
+        },
+    },
+}
+
+
+
+function Description() {
+  return (
+    <motion.div
+    initial='offscreen'
+    whileInView='onscreen'
+    viewport={{ once: true, amount: 0.8 }}
+    variants={descriptionVariants}
+    >
+        <div class="row-span-2 text-left w-full mx-0  mt-64 text-4xl text-white font-montserrat font-extrabold tracking-wide ...">
+            <h1 class='border-l-2 border-blue-900 pl-3'>PROOF OF WORK</h1>
+            <div class=" text-left w-full  text-xl font-montserra font-light tracking-widest ...">
+                <h3 class='leading-loose text-white/50 mr-64 mt-8'>
+                    I specialize in full-stack development. I also like to dabble in data science and machine learning.
+                    Some of the tools I use are:
+                    <span class=' block leading-loose text-white/70 font-bold'>React, Next.js, TypeScript, and Python</span>
+                    <span class=' block leading-loose text-white/70 font-bold'>Node.js, Express.js, MongoDB</span>
+                    Currently working at <a class='underline decoration-blue-900 hover:text-blue-900 visited:text-purple-600' href='https://www.redventures.com/' target='_blank'>Red Ventures Puerto Rico</a> as a Software Engineer Intern.
+                </h3>
+            </div>
+        </div>
+    </motion.div>
+  )
+}
+
+export default Description
